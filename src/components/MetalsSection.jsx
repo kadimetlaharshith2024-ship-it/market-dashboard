969691c9b1eeff243862f1fa4be03b75db8ffeb0
Harshith Reddy <kadimetla.harshith2024@vitstudent.ac.in>
2026-06-30 20:27:18 +0530

    Display fallback message for unavailable metal news

--- src/components/MetalsSection.jsx
+++ src/components/MetalsSection.jsx
@@ -15,18 +15,28 @@ function MetalsSection({ metals, news }) {
 
           <h4>Latest News</h4>
 
-          {news?.goldNews?.slice(0, 2).map((article, index) => (
-            <div key={index} className="news-item">
-              <a
-                    href={article.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="news-link"
-                  >
+          {news?.goldNews?.length > 0 ? (
+
+            news.goldNews.slice(0, 2).map((article, index) => (
+              <div key={index} className="news-item">
+                <a
+                  href={article.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="news-link"
+                >
                   {article.title}
-                  </a>
+                </a>
+              </div>
+            ))
+
+          ) : (
+
+            <div className="news-item">
+              <p>No latest news available.</p>
             </div>
-          ))}
+
+          )}
 
         </div>
 
@@ -38,18 +48,28 @@ function MetalsSection({ metals, news }) {
 
           <h4>Latest News</h4>
 
-          {news?.silverNews?.slice(0, 2).map((article, index) => (
-            <div key={index} className="news-item">
-              <a
-                    href={article.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="news-link"
-                  >
+          {news?.silverNews?.length > 0 ? (
+
+            news.silverNews.slice(0, 2).map((article, index) => (
+              <div key={index} className="news-item">
+                <a
+                  href={article.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="news-link"
+                >
                   {article.title}
-                  </a>
+                </a>
+              </div>
+            ))
+
+          ) : (
+
+            <div className="news-item">
+              <p>No latest news available.</p>
             </div>
-          ))}
+
+          )}
 
         </div>
 
@@ -61,18 +81,28 @@ function MetalsSection({ metals, news }) {
 
           <h4>Latest News</h4>
 
-          {news?.copperNews?.slice(0, 2).map((article, index) => (
-            <div key={index} className="news-item">
-              <a
-                    href={article.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="news-link"
-                  >
+          {news?.copperNews?.length > 0 ? (
+
+            news.copperNews.slice(0, 2).map((article, index) => (
+              <div key={index} className="news-item">
+                <a
+                  href={article.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="news-link"
+                >
                   {article.title}
-                  </a>
+                </a>
+              </div>
+            ))
+
+          ) : (
+
+            <div className="news-item">
+              <p>No latest news available.</p>
             </div>
-          ))}
+
+          )}
 
         </div>
 
@@ -84,18 +114,28 @@ function MetalsSection({ metals, news }) {
 
           <h4>Latest News</h4>
 
-          {news?.aluminumNews?.slice(0, 2).map((article, index) => (
-            <div key={index} className="news-item">
-              <a
-                    href={article.url}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="news-link"
-                  >
+          {news?.aluminumNews?.length > 0 ? (
+
+            news.aluminumNews.slice(0, 2).map((article, index) => (
+              <div key={index} className="news-item">
+                <a
+                  href={article.url}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="news-link"
+                >
                   {article.title}
-                  </a>
+                </a>
+              </div>
+            ))
+
+          ) : (
+
+            <div className="news-item">
+              <p>No latest news available.</p>
             </div>
-          ))}
+
+          )}
 
         </div>
 
@@ -122,7 +162,7 @@ function MetalsSection({ metals, news }) {
                     rel="noreferrer"
                     className="news-link"
                   >
-                  {article.title}
+                    {article.title}
                   </a>
                 </div>
               ))
